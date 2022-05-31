bb9cd5b9fc7c8f8a0941e84c70d63f65e05a1d6d
chirstiankaikai <ckaikai19@gmail.com>
2022-05-31 14:01:03 -0400

    homepage boiler code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,8 @@
 console.log("hello world")
+
+const FEATURED_API = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
+
+
+fetch(FEATURED_API)
+  .then(response => response.json())
+  .then(data => console.log(data));
