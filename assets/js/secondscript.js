584249f902889ddd8abe7d3228195531275eb6ff
chirstiankaikai <ckaikai19@gmail.com>
2022-06-04 19:10:58 -0400

    iushs

--- assets/js/secondscript.js
+++ assets/js/secondscript.js
@@ -1,49 +1,24 @@
-var secondImage = document.getElementById("second-page-image")
-var secondTitle = document.getElementById("second-page-title")
-var similarMovie = "/discover/movie?with_genres=18&sort_by=popularity.desc"
+const secondImage = document.getElementById("second-page-image")
+const secondTitle = document.getElementById("second-page-title")
+const similarMovie = "/discover/movie?with_genres=18&sort_by=popularity.desc"
 const api = "https://api.themoviedb.org/3";
 const key = "&api_key=04c35731a5ee918f014970082a0088b1&page=1";
-var movieSecondPage = localStorage.getItem("title");
-var backButton = document.getElementById("back-button")
-var ratingsText = document.getElementById("rating-text");
-var ratingsBubble = document.getElementById("locks");
-// var icon = document.getElementByid("mesh fa-solid fa-comment")
+const movieSecondPage = localStorage.getItem("title");
+const backButton = document.getElementById("back-button")
+const ratingsText = document.getElementById("rating-text");
+const ratingsBubble = document.getElementById("locks");
 
-// function loadData (data){
-//     console.log(data)
-//     var dataObject = JSON.parse(localStorage.getItem("results"))
-//     console.log(dataObject);
-//     window.location.href = "secondpage.html"
-//     console.log(dataObject[0].original_title)
-//     console.log(dataObject[0].poster_path)
-//     secondImage.src = dataObject[0].poster_path
-//     secondTitle.textContent = dataObject[0].original_title
-// }
-
-// loadData();
 
 function getOMDBData(){
     var movieSecondPage = localStorage.getItem("title");
     var requestUrlImage = "https://www.omdbapi.com/?apikey=84b19fcd&t=" + movieSecondPage
 
-
   fetch(requestUrlImage)
-    .then(function(responseImage){
-      return responseImage.json(); 
-  })
-  .then(function(data){
-      // console.log(data)
-    //   var dataObject = JSON.parse(localStorage.getItem("results"))
-    //   console.log(dataObject);
-    //   window.location.href = "secondpage.html"
-    //   console.log(dataObject[0].original_title)
-    //   console.log(dataObject[0].poster_path)
+    .then((responseImage)=>{return responseImage.json()})
+    .then((data)=>{
 
-
-
-      // secondImage.src = data.Poster
       secondTitle.innerText = movieSecondPage;
-      document.getElementById("release-date").innerText = data.Year;
+      document.getElementById("release-date").innerText = data.Year.slice(0, 4);;
       document.getElementById("ratt").innerText = data.Rated;
       document.getElementById("mis").innerText = data.Plot;
       document.getElementById("gene").innerText = data.Genre;
@@ -56,37 +31,38 @@ function getOMDBData(){
       document.getElementById("money").innerText = data.BoxOffice;
       document.getElementById("run").innerText = data.Runtime;
       document.getElementById("type").innerText = data.Type;
-      secondImage.setAttribute("src", data.Poster);
-      console.log(data.Poster);
-      console.log(data.Metascore)
+      
+      if(data.Poster){
+        secondImage.setAttribute("src", data.Poster);
+      } else {
+        console.log('no poster');
+      }
 
       if(data.Ratings.length != 3){
         ratingsBubble.style.display = "none"
-        console.log("ok")
+
       } else {
-        console.log(data.imdbRating)
-      
         let metaValue = data.Ratings[1].Value.substring(0, 2);
         imdbValue = data.imdbRating * 10
-        console.log(imdbValue)
         averageValue = parseFloat(imdbValue) + parseFloat(metaValue) + parseFloat(data.Metascore)
         finalAverage = (averageValue / 3)
-        console.log(finalAverage)
         ratingsText.innerText = Math.round(finalAverage)
-        console.log(averageValue)
-        console.log(finalAverage)
     
         if(finalAverage > 80){
-          console.log("good")
           ratingsBubble.style.color = "green"
+          document.getElementById("second-page-image").setAttribute("class", "movie-selected-poster good");
+
+
         } else if (finalAverage < 55){
-          console.log("poor")
           ratingsBubble.style.color = 'red'
+          document.getElementById("second-page-image").setAttribute("class", "movie-selected-poster poor");
+
         } else if (finalAverage > 55 && finalAverage < 80){
-          console.log("medium")
           ratingsBubble.style.color = "orange"
+          document.getElementById("second-page-image").setAttribute("class", "movie-selected-poster medium");
+
+
         } else {
-        console.log("average rating not possible")
         ratingsBubble.style.display = "none"
         ratingsText.innerText = ""
         } 
@@ -103,121 +79,162 @@ function getIMDBData(){
     .then((response)=>{return response.json()})
     .then((data) => {
       let results = data.results;
+      let data_id;
       
       for(let i=0; i < results.length; i++){
         if(chosen_title == results[i].title){
-          document.getElementById("blueigdiud").style.backgroundImage = `url("${img_path + results[i].backdrop_path}")`;
+          if(results[i].backdrop_path){
+            document.getElementById("blueigdiud").style.backgroundImage = `url("${img_path + results[i].backdrop_path}")`;
+            document.getElementById("load").style.backgroundImage = `url("${img_path + results[i].backdrop_path}")`;
+          }
           document.getElementById("dis").innerText = results[i].overview;
+          data_id = results[i].id;
         }
       }
-
-      byGenre();
+      getTrailer(data_id);
+      getComments(data_id);
     });
 }
 
-
 function byGenre(){
-  const pages = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
-  var page = pages[Math.floor(Math.random() * pages.length)];
+  const pages = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
+  const page = pages[Math.floor(Math.random() * pages.length)];
   const img_path = "https://image.tmdb.org/t/p/w1280";
   const api = "https://api.themoviedb.org/3";
   const key = `&api_key=04c35731a5ee918f014970082a0088b1&page=${page}`;
-  const most_popular_query = "/discover/movie?sort_by=popularity.desc"
-
+  const most_popular_query = "/discover/movie?sort_by=popularity.desc";
 
   fetch(api + most_popular_query + key)
     .then((response)=>{return response.json()})
     .then((data)=>{
       let results = data.results;
-      console.log(results)
-      document.getElementById("scroll-1").setAttribute("src", img_path + results[0].poster_path)
-      document.getElementById("scroll-2").setAttribute("src", img_path + results[1].poster_path)
-      document.getElementById("scroll-3").setAttribute("src", img_path + results[2].poster_path)
-      document.getElementById("scroll-4").setAttribute("src", img_path + results[3].poster_path)
-      document.getElementById("scroll-5").setAttribute("src", img_path + results[4].poster_path)
-      document.getElementById("scroll-6").setAttribute("src", img_path + results[5].poster_path)
-      document.getElementById("scroll-7").setAttribute("src", img_path + results[6].poster_path)
-      document.getElementById("scroll-8").setAttribute("src", img_path + results[7].poster_path)
-      document.getElementById("scroll-9").setAttribute("src", img_path + results[8].poster_path)
-      document.getElementById("scroll-10").setAttribute("src", img_path + results[9].poster_path)
+      
+      document.getElementById("scroll-1").setAttribute("src", img_path + results[0].poster_path);
+      document.getElementById("scroll-2").setAttribute("src", img_path + results[1].poster_path);
+      document.getElementById("scroll-3").setAttribute("src", img_path + results[2].poster_path);
+      document.getElementById("scroll-4").setAttribute("src", img_path + results[3].poster_path);
+      document.getElementById("scroll-5").setAttribute("src", img_path + results[4].poster_path);
+      document.getElementById("scroll-6").setAttribute("src", img_path + results[5].poster_path);
+      document.getElementById("scroll-7").setAttribute("src", img_path + results[6].poster_path);
+      document.getElementById("scroll-8").setAttribute("src", img_path + results[7].poster_path);
+      document.getElementById("scroll-9").setAttribute("src", img_path + results[8].poster_path);
+      document.getElementById("scroll-10").setAttribute("src", img_path + results[9].poster_path);
       document.getElementById("scroll-11").setAttribute("src", img_path + results[10].poster_path);
       document.getElementById("scroll-12").setAttribute("src", img_path + results[11].poster_path);
       document.getElementById("scroll-13").setAttribute("src", img_path + results[12].poster_path);
       document.getElementById("scroll-14").setAttribute("src", img_path + results[13].poster_path);
       document.getElementById("scroll-15").setAttribute("src", img_path + results[14].poster_path);
   })
-
 } 
+
+function getTrailer(id){
+  const video_api = `https://api.themoviedb.org/3/movie/${id}/videos?api_key=04c35731a5ee918f014970082a0088b1`;
+  const video_path = "https://www.youtube.com/embed/";
+  const auto_play = "?autoplay=1";
+  // const auto_play = "";
+  const mute = "&mute=1"
+
+  fetch(video_api)
+    .then((response)=>{return response.json()})
+    .then((data)=>{
+      
+      if(data.results == []){
+        document.getElementById("video").style.display = "none";
+      } else {
+        let video = video_path + data.results[0].key + auto_play + mute + "&loop=1" + "&modestbranding=1&autohide=1&showinfo=0&controls=1";
+        document.getElementById("video").setAttribute("src", video);
+        document.getElementById("video").style.display = "block";
+      }
+    })
+}
+
+function getComments(id){
+  const api = `https://api.themoviedb.org/3/movie/${id}/reviews?api_key=04c35731a5ee918f014970082a0088b1&page=1`;
+
+  fetch(api)
+  .then((response)=>{return response.json()})
+  .then((data)=>{
+    let results = data.results;
+
+    if(results[0]){
+      document.getElementById("first-username").innerText = results[0].author_details.username;
+      document.getElementById("comment-content-1").innerHTML = results[0].content;
+    }
+
+    if(results[1]){
+      document.getElementById("username-2").innerText = results[1].author_details.username;
+      document.getElementById("comment-content-2").innerHTML = results[1].content;
+    } 
+
+    if(results[2]){
+      document.getElementById("username-3").innerText = results[2].author_details.username;
+      document.getElementById("comment-content-3").innerHTML = results[2].content;
+    } 
+
+    if(results[3]){
+      document.getElementById("username-4").innerText = results[3].author_details.username;
+      document.getElementById("comment-content-4").innerHTML = results[3].content;
+    } 
+  })
+}
 
 function toHomepage(event){
   event.preventDefault();
   window.location.href = "../../index.html";
 }
 
-function init(){
-  getOMDBData();
-  getIMDBData();
+function sideDrag(e){
+  const slider = document.getElementById("items");
+
+  let isDown = false;
+  let startX;
+  let scrollLeft;
+  
+  slider.addEventListener('mousedown', (e) => {
+    isDown = true;
+    slider.classList.add('active');
+    startX = e.pageX - slider.offsetLeft;
+    scrollLeft = slider.scrollLeft;
+  });
+  slider.addEventListener('mouseleave', () => {
+    isDown = false;
+    slider.classList.remove('active');
+  });
+  slider.addEventListener('mouseup', () => {
+    isDown = false;
+    slider.classList.remove('active');
+  });
+  
+  slider.addEventListener('mousemove', (e) => {
+    if(!isDown) return;
+    e.preventDefault();
+    const x = e.pageX - slider.offsetLeft;
+    const walk = (x - startX) * 3; //scroll-fast
+    slider.scrollLeft = scrollLeft - walk;
+    console.log(walk);
+  });
+  
+
 }
 
-backButton.addEventListener("click", toHomepage);
+function load(){
+  window.addEventListener('load', () => {
+    setTimeout(()=>{
+        console.log("display elements")
+        document.getElementById("blueigdiud").style.display = 'block';
+        document.getElementById("content").style.display = 'block';
+        document.getElementById("load").style.display = 'none';
+    }, 2000)
+  });
+}
+
+function init(){
+  load();
+  getOMDBData();
+  getIMDBData();
+  byGenre();
+  sideDrag();
+  backButton.addEventListener("click", toHomepage);
+}
+
 init();
-
-
-        
-
-          
-      
-//   })
-// }
-
-
-// function displaySimilarMovies(){
-
-//     fetch(api + similarMovie + key)
-//     .then(response => response.json())
-//     .then(data => {
-//       let results = data.results;
-//       console.log(results)
-//     });
-//   }
-
-//   displaySimilarMovies();
-
-
-
-
-//   Action & Adventure  10759
-// Animation           16
-// Comedy              35
-// Crime               80
-// Documentary         99
-// Drama               18
-// Family              10751
-// Kids                10762
-// Mystery             9648
-// News                10763
-// Reality             10764
-// Sci-Fi & Fantasy    10765
-// Soap                10766
-// Talk                10767
-// War & Politics      10768
-// Western             37
-// Action          28
-// Adventure       12
-// Animation       16
-// Comedy          35
-// Crime           80
-// Documentary     99
-// Drama           18
-// Family          10751
-// Fantasy         14
-// History         36
-// Horror          27
-// Music           10402
-// Mystery         9648
-// Romance         10749
-// Science Fiction 878
-// TV Movie        10770
-// Thriller        53
-// War             10752
-// Western         37
